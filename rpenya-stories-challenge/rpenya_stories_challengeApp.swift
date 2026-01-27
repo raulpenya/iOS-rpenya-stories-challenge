ce5f3e89990e2f07c0a69cba0cce9afa056d7976
rpenya <rpenya.mail@gmail.com>
2026-01-27 14:22:09 +0100

    initial commit

---
+++ rpenya-stories-challenge/rpenya_stories_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  rpenya_stories_challengeApp.swift
+//  rpenya-stories-challenge
+//
+//  Created by Raul Peña on 27/1/26.
+//
+
+import SwiftUI
+
+@main
+struct rpenya_stories_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
